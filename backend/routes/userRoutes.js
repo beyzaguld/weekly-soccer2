4c477e7a715a00efb1dbc6aa447295f25431d189
Beyzagül Demir <85847630+beyzaguld@users.noreply.github.com>
2022-11-02 16:11:49 +0300

    Add files via upload

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  authUser,
+  registerUser,
+  updateUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
+const router = express.Router();
+
+router.route("/").post(registerUser);
+router.route("/login").post(authUser);
+router.route("/profile").post(protect, updateUserProfile);
+
+
+export default router;
